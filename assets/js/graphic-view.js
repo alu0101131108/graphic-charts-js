71b81b5b47b4208c5973e573bc5a12e7684cad99
Sebastian <alu0101131108@ull.edu.es>
2021-06-18 00:33:04 +0100

    data on basic charts and back button

--- assets/js/graphic-view.js
+++ assets/js/graphic-view.js
@@ -1,8 +1,8 @@
 /**
  * @author Sebastián Daniel Tamayo Guzmán
- * @since 11/06/2021
- * @desc  Requests data via AJAX from a php server in order to
- *        show it inside tables.
+ * @since 17/06/2021
+ * @desc  Creates a graphic view app with the data retrieved from
+ * a concrete device.
  * @module GraphicData
  */
 'use strict';
@@ -16,14 +16,17 @@ class GraphicView {
     this.backup = this.root.innerHTML;
     this.root.innerHTML = '';
     this.canvas = document.createElement('canvas');
-    this.canvas.width = 800;
-    this.canvas.height = 450;
     this.requestData(sn);
     this.root.appendChild(this.canvas);
-    setTimeout(() => {
+
+    
+    const BUTTON = document.createElement('button');
+    BUTTON.textContent = 'Back to table';
+    BUTTON.onclick = () => {
       this.root.innerHTML = this.backup;
       tableClickEvents();
-    }, 5000);
+    }
+    this.root.appendChild(BUTTON);
   }
 
   requestData(target) {
@@ -37,34 +40,40 @@ class GraphicView {
       },
       dataType: 'JSON',
       success: (response) => {
-        const OLT_RX = [];
-        const ONU_RX = [];
-        const HORA = [];
+        const OLT_RXS = [];
+        const ONU_RXS = [];
+        const HORAS = [];
         for (let row of response) {
-          OLT_RX.push(row['OLT_RX']);
-          ONU_RX.push(row['ONU_RX']);
-          HORA.push(row['HORA']);
+          OLT_RXS.push(parseFloat(row['OLT_RX']));
+          ONU_RXS.push(parseFloat(row['ONU_RX']));
+          HORAS.push(new Date(row['HORA']));
         }
-        this.setCharts([OLT_RX, ONU_RX, HORA]);
+        const DATA = {
+          OLT_RX: OLT_RXS,
+          ONU_RX: ONU_RXS,
+          HORA: HORAS
+        };
+        this.setCharts(DATA);
       }
     });
   }
 
   setCharts(data) {
-    console.log(data);
     new Chart(this.canvas, {
       type: 'line',
       data: {
-        labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+        labels: data['HORA'].map((elem) => { 
+          return elem.toLocaleTimeString(); 
+        }),
         datasets: [{ 
-            data: [86,114,106,106,107,111,133,221,783,2478],
-            label: "Africa",
-            borderColor: "#3e95cd",
+            data: data['OLT_RX'],
+            label: 'OLT_RX',
+            borderColor: '#3e95cd',
             fill: false
           }, { 
-            data: [282,350,411,502,635,809,947,1402,3700,5267],
-            label: "Asia",
-            borderColor: "#8e5ea2",
+            data: data['ONU_RX'],
+            label: 'ONU_RX',
+            borderColor: '#8e5ea2',
             fill: false
           }
         ]
@@ -72,7 +81,7 @@ class GraphicView {
       options: {
         title: {
           display: true,
-          text: 'World population per region (in millions)'
+          text: 'Device Data Chart'
         }
       }
     });
